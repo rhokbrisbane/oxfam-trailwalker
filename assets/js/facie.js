68728a7caaa38100081da67a53d2238f52ce5a09
Steve Shearn <steve@syntronian.com>
2016-11-27 11:36:20 +1000

    fb share using dialog

--- assets/js/facie.js
+++ assets/js/facie.js
@@ -1,3 +1,10 @@
-function fbShare(permalinkParam){
-    window.location.replace('https://www.facebook.com/sharer/sharer.php?u=findmeawalk.com' + permalinkParam);
+function fbShare(permalinkParam) {
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            FB.ui({
+                method: 'share',
+                href: 'https://www.facebook.com/sharer/sharer.php?u=findmeawalk.com#' + permalinkParam,
+            }, function(response){});
+        }
+    });
 }
