c4144e69716994a2babc0b24d05451ce665fa026
Steve Shearn <steve@syntronian.com>
2016-11-27 11:54:56 +1000

    prompt fb login prior to share, debug/prod fb app id switch based on domain

--- assets/js/facie.js
+++ assets/js/facie.js
@@ -1,10 +1,22 @@
 function fbShare(permalinkParam) {
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
-            FB.ui({
-                method: 'share',
-                href: 'https://www.facebook.com/sharer/sharer.php?u=findmeawalk.com#' + permalinkParam,
-            }, function(response){});
+            fbShareDialog(permalinkParam);
+        } else {
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    FB.api('/me', function(response) {                        
+                        fbShareDialog(permalinkParam);
+                    });
+                }
+            });
         }
     });
+}
+
+function fbShareDialog(permalinkParam) {
+    FB.ui({
+        method: 'share',
+        href: 'https://findmeawalk.com#' + permalinkParam,
+    }, function(response){});
 }
