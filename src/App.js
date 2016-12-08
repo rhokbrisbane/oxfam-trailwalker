93db9286c4499ce512b038027381ef8fed53c212
Seb Patane <seb@libero.com.au>
2016-12-09 01:22:07 +1000

    Wire up the display for the trail name, distance and difficulty * Use the trail name as a placeholder to display the loading text * Just hide the distance and difficulty fields unless we have data for them

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ type Walk = {
 // Constants
 const ROUTE_LENGTHENING_PERCENTAGE = 1.5;
 const DEFAULT_ROUTE_TARGET_LENGTH = 5 /* km */;
+const DEFAULT_TRAIL_NAME = "Yours to discover!";
 
 // TODO: make these dynamic, calculated by the shortest/longest walks in the available dataset
 const MINIMUM_ROUTE_LENGTH = 0.5;
@@ -50,43 +51,41 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div id="loading"><h1>Finding Walks...</h1></div>
-        <div className="site-container">
+      <div className="site-container">
+          <div className="filter-search-box">
+              <div className="filter-search-logo">
+                  <h1>Find Me A Walk<img src={headerIcon} alt="Logo" /></h1>
+              </div>
+              <div className="filter-search-track-details">
+                  <span className="filter-search-name">{ this.state.currentWalk ? (this.state.currentWalk.trailName || DEFAULT_TRAIL_NAME) : "Finding Walks..." }</span>
+                  { this.state.currentWalk && (<span className="filter-search-length">{this.state.currentWalk.distance}</span>) }
+                  { this.state.currentWalk && (<span className="filter-search-difficulty">{this.state.currentWalk.difficulty}</span>) }
+              </div>
+              <div className="filter-map-details">
+              </div>
+          </div>
 
-            <div className="filter-search-box">
-                <div className="filter-search-logo">
-                    <h1>Find Me A Walk<img src={headerIcon} alt="Logo" /></h1>
-                </div>
-                <div className="filter-search-track-details">
-                    <span id="trailName" className="filter-search-name">Track Name</span><span id="length" className="filter-search-length">Distance</span><span id='trailDifficutly' className="filter-search-difficulty">Difficulty</span>
-                </div>
-                <div className="filter-map-details">
-                </div>
-            </div>
+          <div className="filter-social-icons">
+              <a href="https://www.facebook.com/Find-Me-A-Walk-1610148005948849/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+              <a href="https://www.instagram.com/findmeawalk/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+              <a href="https://twitter.com/FindMeAWalk" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="https://github.com/rhokbrisbane/oxfam-trailwalker/" target="_blank" rel="noopener noreferrer" title="Help improve this app!"><i className="fa fa-github" aria-hidden="true"></i></a>
+              <div className="filter-links">
+                  <a href="http://blog.findmeawalk.com/" target="_blank"><span>Blog</span></a>
+                  <a href="https://trailwalker.oxfam.org.au/" target="_blank"><span>Inspired by Oxfam Trailwalker</span></a>
+              </div>
+          </div>
 
-            <div className="filter-social-icons">
-                <a href="https://www.facebook.com/Find-Me-A-Walk-1610148005948849/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a>
-                <a href="https://www.instagram.com/findmeawalk/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="https://twitter.com/FindMeAWalk" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-                <a href="https://github.com/rhokbrisbane/oxfam-trailwalker/" target="_blank" rel="noopener noreferrer" title="Help improve this app!"><i className="fa fa-github" aria-hidden="true"></i></a>
-                <div className="filter-links">
-                    <a href="http://blog.findmeawalk.com/" target="_blank"><span>Blog</span></a>
-                    <a href="https://trailwalker.oxfam.org.au/" target="_blank"><span>Inspired by Oxfam Trailwalker</span></a>
-                </div>
-            </div>
+          <div className="filter-map-buttons">
+              <button className="submit lets-do-it">Find Friends!</button>
+              <div className="other-buttons">
+                  <button className="length" onClick={this.makeTargetLengthLonger} >Too Short</button>
+                  <button className="length" onClick={this.makeTargetLengthShorter} >Too Long</button>
+              </div>
+          </div>
 
-            <div className="filter-map-buttons">
-                <button className="submit lets-do-it">Find Friends!</button>
-                <div className="other-buttons">
-                    <button className="length" onClick={this.makeTargetLengthLonger} >Too Short</button>
-                    <button className="length" onClick={this.makeTargetLengthShorter} >Too Long</button>
-                </div>
-            </div>
+          <Map center={this.state.currentLocation} defaultZoom={5} />
 
-            <Map center={this.state.currentLocation} defaultZoom={5} />
-
-        </div>
       </div>
     );
   }
