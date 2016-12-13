208ab218815458be68be9cda9dc029475393afdd
Seb Patane <seb@libero.com.au>
2016-12-13 21:23:03 +1000

    Ensure Google Maps is definitely loaded before React * Prior to this there was a race condition where on slow connections Google maps wouldn't have loaded fast enough, leading to a google is undefined error * Now we we call the ReactDOM.render() function from the google maps loaded callback, so react doesn't even start until google maps has loaded   * Google maps is now loaded via scriptjs in index.js, since this allows us to interpolate the api key as well as ensure the callback exists for it to call * Also simplified the Map component, which still needs to have its props set at runtime rather than load time   * This led to a bug in flow regarding type variances, which has been worked around but will hopefully be fixed soon

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,192 +3,176 @@
 
 import React, { Component } from 'react';
 import { GoogleMap, withGoogleMap } from 'react-google-maps';
-import withScriptjs from 'react-google-maps/lib/async/withScriptjs';
 
-export type Coordinates = {lat: number, lng: number}
+export type Coordinates = { lat: number, lng: number }
 declare var google: Object;
-
-type Props = {
-  options?: Object
-}
 
 export const GOOGLE_MAPS_API_KEY = "AIzaSyD8MgbUf8RGv05GJ6qjrPLRg3Fvb4HTA9k";
 
 // Initialize the higher order components and set their defaults below
-const MapBoilerplate = withScriptjs(withGoogleMap(class extends Component {
-  props: Props
+const MainMap = withGoogleMap(class extends Component {
 
-  state: {
-    mapOptions?: Object
+  // TODO: Remove after https://github.com/facebook/flow/issues/3008 is resolved
+  static set defaultProps(dummy) {
+    // zzz
   }
 
-  constructor(props: Props) {
-    super(props)
-
-    this.state = {
-      mapOptions: {}
-    }
-  }
-
-  componentDidMount() {
-    // We need to wait for the google api to load before we can use the google.maps constants
-    const DEFAULT_GOOGLE_MAP_OPTIONS = {
-      disableDefaultUI: true,
-      draggable: true,
-      scrollwheel: true,
-      panControl: false,
-      mapTypeControl: false,
-      scaleControl: false,
-      styles: [{
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#f5f5f5"
-              }]
+  static get defaultProps() {
+    return {
+      defaultZoom: 3,
+      defaultCenter: { lat: -25.363882, lng: 131.044922 },
+      options: {
+        disableDefaultUI: true,
+        draggable: true,
+        scrollwheel: true,
+        panControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        styles: [
+          {
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#f5f5f5"
+            }]
           },
           {
-              "elementType": "labels.icon",
-              "stylers": [{
-                  "visibility": "off"
-              }]
+            "elementType": "labels.icon",
+            "stylers": [{
+              "visibility": "off"
+            }]
           },
           {
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#616161"
-              }]
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#616161"
+            }]
           },
           {
-              "elementType": "labels.text.stroke",
-              "stylers": [{
-                  "color": "#f5f5f5"
-              }]
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#f5f5f5"
+            }]
           },
           {
-              "featureType": "administrative.land_parcel",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#bdbdbd"
-              }]
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#bdbdbd"
+            }]
           },
           {
-              "featureType": "poi",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#eeeeee"
-              }]
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#eeeeee"
+            }]
           },
           {
-              "featureType": "poi",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#757575"
-              }]
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#757575"
+            }]
           },
           {
-              "featureType": "poi.park",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#e5e5e5"
-              }]
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#e5e5e5"
+            }]
           },
           {
-              "featureType": "poi.park",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#9e9e9e"
-              }]
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#9e9e9e"
+            }]
           },
           {
-              "featureType": "road",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#ffffff"
-              }]
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#ffffff"
+            }]
           },
           {
-              "featureType": "road.arterial",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#757575"
-              }]
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#757575"
+            }]
           },
           {
-              "featureType": "road.highway",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#dadada"
-              }]
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#dadada"
+            }]
           },
           {
-              "featureType": "road.highway",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#616161"
-              }]
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#616161"
+            }]
           },
           {
-              "featureType": "road.local",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#9e9e9e"
-              }]
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#9e9e9e"
+            }]
           },
           {
-              "featureType": "transit.line",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#e5e5e5"
-              }]
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#e5e5e5"
+            }]
           },
           {
-              "featureType": "transit.station",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#eeeeee"
-              }]
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#eeeeee"
+            }]
           },
           {
-              "featureType": "water",
-              "elementType": "geometry",
-              "stylers": [{
-                  "color": "#c9c9c9"
-              }]
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#c9c9c9"
+            }]
           },
           {
-              "featureType": "water",
-              "elementType": "labels.text.fill",
-              "stylers": [{
-                  "color": "#9e9e9e"
-              }]
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#9e9e9e"
+            }]
           }
-      ],
-      zoomControl: true,
-      zoomControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_CENTER
-      },
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+        ],
+        zoomControl: true,
+        zoomControlOptions: {
+          position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
     }
-
-    this.setState({mapOptions: DEFAULT_GOOGLE_MAP_OPTIONS})
   }
 
   render() {
     return (<GoogleMap
       {...this.props}
-      options={{...this.state.mapOptions, ...this.props.options}}
-    />) 
+      />)
   }
-}));
+});
 
-// Fill in enough defaults to get the map working
-MapBoilerplate.defaultProps = {
-  googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}`,
-  defaultZoom: 3,
-  defaultCenter: { lat: -25.363882, lng: 131.044922 },
-  loadingElement: (<div />),
+// These are set separately because they apply to the withGoogle HOC
+MainMap.defaultProps = {
   containerElement: (<div style={{ height: `100%` }} />),
   mapElement: (<div id="map" />)
 }
 
-export default MapBoilerplate;
+export default MainMap;
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
+import scriptjs from 'scriptjs';
+import { GOOGLE_MAPS_API_KEY } from './components/Map';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// Don't render until we have google maps loaded
+window.whenGoogleMapsIsLoaded = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+// Now load google maps
+scriptjs(`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=whenGoogleMapsIsLoaded`)
